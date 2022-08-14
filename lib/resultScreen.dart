ab46db20fdc7c906dba9f6d55fc6aa67f1c7d151
yasser74220 <yasserahlawy970@gmail.com>
2022-08-14 16:40:43 +0200

    Final

---
+++ lib/resultScreen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+class resultScreen extends StatelessWidget {
+ final int age ;
+final double height;
+final int result;
+final bool isMale;
+
+  resultScreen({
+
+   required this.age, required this.height, required this.result, required this.isMale});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Result"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Gender : ${isMale ? 'Male' : 'Female'}',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Result : $result',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Age : $age',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
